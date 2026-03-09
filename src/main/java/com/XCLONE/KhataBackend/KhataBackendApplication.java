a3641ff463d63ca9dc848f5e2c2d9e518f839f0f
Ankit Singh <114000644+ankitsingh0913@users.noreply.github.com>
2026-03-09 23:28:53 +0530

    Interval

--- src/main/java/com/XCLONE/KhataBackend/KhataBackendApplication.java
+++ src/main/java/com/XCLONE/KhataBackend/KhataBackendApplication.java
@@ -9,5 +9,7 @@ public class KhataBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KhataBackendApplication.class, args);
 	}
-
+	/*
+	Nothing much to say here, just the main application class that bootstraps the Spring Boot application. The @SpringBootApplication annotation is a convenience annotation that adds all of the following:
+	 */
 }
