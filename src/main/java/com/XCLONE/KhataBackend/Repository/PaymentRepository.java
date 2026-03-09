fce90edbd9002be71811794077f041c7defaf9c1
Ankit Singh <114000644+ankitsingh0913@users.noreply.github.com>
2026-03-09 22:47:20 +0530

    Payment Methods

--- src/main/java/com/XCLONE/KhataBackend/Repository/PaymentRepository.java
+++ src/main/java/com/XCLONE/KhataBackend/Repository/PaymentRepository.java
@@ -1,12 +1,14 @@
 package com.XCLONE.KhataBackend.Repository;
 
 import com.XCLONE.KhataBackend.Entity.Payment;
-import com.XCLONE.KhataBackend.enums.PaymentType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-    List<Payment> findCustomerId(UUID customerId);
+@Repository
+public interface PaymentRepository extends JpaRepository<Payment, UUID> {
+    List<Payment> findByCustomerId(UUID customerId);
+    List<Payment> findByUserId(UUID userId);
 }
